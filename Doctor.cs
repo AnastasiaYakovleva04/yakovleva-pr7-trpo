7d4a0405bbec7f45f4683ac9de8eee50e0a7e054
Anastasia <nastasiakovleva@gmail.com>
2025-10-28 21:16:04 +0500

    работа с пользователем

---
+++ Doctor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace yakovleva_pr7
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronimic { get; set; }
+        public string Specialization { get; set; }
+        public string Password { get; set; }
+        
+        private Dictionary<int, string> ids = new Dictionary<int, string>();
+        private static bool isLoaded = false;
+        public Doctor() { }
+
+        public Random rnd = new Random();
+
+        public void LoadDoctors()
+        {
+            string[] doctorFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "D_*.json");
+
+            foreach (string file in doctorFiles)
+            {
+                string jsonString = File.ReadAllText(file);
+                Doctor doctor = JsonSerializer.Deserialize<Doctor>(jsonString);
+                ids[doctor.Id] = doctor.Password;
+            }
+        }
+        public void Registration(string name, string surname, string patronimic, string specialization, string password, string confPass)
+        {
+            if (name == "" || surname == "" || patronimic == "" || specialization == "" || password == "" || confPass == "")
+                throw new ArgumentException("Все поля должны быть заполнены");
+            
+            if (password != confPass)
+                throw new ArgumentException ("Пароли не совпадают");
+              
+            int id;
+            do { id = rnd.Next(10000, 100000); }
+            while (ids.ContainsKey(id));
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Patronimic = patronimic;
+            Specialization = specialization;
+            Password = password;
+            ids[id] = password;
+            
+        }
+
+        public void Login(string id, string password)
+        {
+            if (id == "" && password == "")
+                throw new ArgumentException("Все поля должны быть заполнены");
+
+            if (!int.TryParse(id, out int ID))
+                throw new FormatException("Некорректный ввод ID");
+
+            if (!ids.ContainsKey(ID) || ids[ID] != password)
+                throw new ArgumentException("Неверный логин или пароль");
+
+
+            var path = $"D_{ID}.json"; 
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Пользователь не найден");
+
+            var jsonString = File.ReadAllText(path);
+            Doctor doctor = JsonSerializer.Deserialize<Doctor>(jsonString);
+
+            this.Id = doctor.Id;
+            this.Name = doctor.Name;
+            this.Surname = doctor.Surname;
+            this.Patronimic = doctor.Patronimic;
+            this.Specialization = doctor.Specialization;
+            this.Password = doctor.Password;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace yakovleva_pr7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Doctor doc;
+
+        public MainWindow()
+        {
+            doc = new Doctor();
+            InitializeComponent();
+            DataContext = doc;
+            doc.LoadDoctors();
+        }
+
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameTextBox.Text;
+            var surname = SurnameTextBox.Text;
+            var patronimic = PatronimicTextBox.Text;
+            var specialization = SpecTextBox.Text;
+            var password = PassTextBox.Text;
+            var confirmPassword = ConfirmPassTextBox.Text;
+            try
+            {
+                doc.Registration(name, surname, patronimic, specialization, password, confirmPassword);
+                MessageBox.Show($"Вы зарегистрированы. Ваш ID = {doc.Id}", "Успешно");
+                var jsonString = JsonSerializer.Serialize(doc);
+                var path = $"D_{doc.Id}.json";
+                File.WriteAllText(path, jsonString, Encoding.UTF8);
+                NameTextBox.Text = "";
+                SurnameTextBox.Text = "";
+                PatronimicTextBox.Text = "";
+                SpecTextBox.Text = "";
+                PassTextBox.Clear();
+                ConfirmPassTextBox.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var id = IDTextBox.Text;
+            var password = PassLoginTextBox.Text;
+            try
+            {
+                doc.Login(id, password);
+                MessageBox.Show("Вход выполнен", "Успешно");
+                IDTextBox.Clear();
+                PassLoginTextBox.Clear();
+                DataContext = null;
+                DataContext = doc;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
